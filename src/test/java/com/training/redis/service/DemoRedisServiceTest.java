9c4e082a6ef2e9c7732aebb21d1329f04c6a245d
LuWenhui <1265400024@qq.com>
2016-06-29 13:36:23 +0800

    更新redis测试

--- src/test/java/com/training/redis/service/DemoRedisServiceTest.java
+++ src/test/java/com/training/redis/service/DemoRedisServiceTest.java
@@ -5,11 +5,19 @@ import java.util.Date;
 import javax.inject.Inject;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 import com.training.redis.entity.DemoRedis;
 import com.xxx.training.BaseTest;
 
-public class DemoRedisServiceTest extends BaseTest {
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+//@ContextConfiguration(locations ={"classpath*:spring-mvc.xml","classpath*:spring-bean.xml","classpath*:spring-jpa.xml","classpath*:spring-druid.xml","classpath*:spring-security.xml","classpath*:spring-quartz.xml","classpath*:spring-redis.xml"})
+@ContextConfiguration(locations ={"classpath*:spring-redis.xml"})
+public class DemoRedisServiceTest {
 
 	@Inject
 	private DemoRedisService demoRedisService;
