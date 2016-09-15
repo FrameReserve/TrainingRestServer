31efc627713aee162123d2d17de2ad9c2e51f9eb
LuWenhui <1265400024@qq.com>
2016-09-15 14:17:39 +0800

    删除jpa base

--- src/main/java/com/training/member/service/impl/MemberInfoServiceImpl.java
+++ src/main/java/com/training/member/service/impl/MemberInfoServiceImpl.java
@@ -1,9 +1,9 @@
 package com.training.member.service.impl;
 
-import com.training.core.service.impl.JpaBaseServiceImpl;
+import com.training.core.service.impl.MyBatisBaseServiceImpl;
 import com.training.member.entity.MemberInfo;
 import com.training.member.service.MemberInfoService;
 
-public class MemberInfoServiceImpl extends JpaBaseServiceImpl<MemberInfo> implements MemberInfoService {
+public class MemberInfoServiceImpl extends MyBatisBaseServiceImpl<MemberInfo> implements MemberInfoService {
 
 }
